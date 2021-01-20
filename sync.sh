d66ce5cd96f3be022828eaf8150c9c8c115ab3d7
Ángel Barrera Sánchez <angel@sighup.io>
2021-01-20 09:24:13 +0100

    Enable multiple destination to the same image

--- sync.sh
+++ sync.sh
@@ -5,10 +5,14 @@ for (( c=0; c<${IMAGES}; c++ ))
 do
     NAME=$(yq e '.images['"${c}"'].name' images.yml)
     SRC=$(yq e '.images['"${c}"'].source' images.yml)
-    DST=$(yq e '.images['"${c}"'].destination' images.yml)
     echo "  - Start ${NAME}"
     docker pull ${SRC}
-    docker tag ${SRC} ${DST}
-    docker push ${DST}
+    DST=$(yq e '.images['"${c}"'].destination | length' images.yml)
+    for (( d=0; d<${DST}; d++ ))
+    do
+        TO=$(yq e '.images['"${c}"'].destination['"${d}"']' images.yml)
+        docker tag ${SRC} ${TO}
+        docker push ${TO}
+    done
     echo "  - Finish ${NAME}"
 done
