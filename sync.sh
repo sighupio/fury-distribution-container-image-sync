b32592ff7c39b120e17d6650de9d60657518d4eb
Luca Zecca <l.zecca78@gmail.com>
2021-06-07 10:18:46 +0200

    destination is an array so it should be plural

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ do
     NAME=$(yq e '.images['"${c}"'].name' images.yml)
     SRC=$(yq e '.images['"${c}"'].source' images.yml)
     echo "  - Start ${NAME}"
-    DST=$(yq e '.images['"${c}"'].destination | length' images.yml)
+    DST=$(yq e '.images['"${c}"'].destinations | length' images.yml)
     TAG=$(yq e '.images['"${c}"'].tag | length' images.yml)
     for (( t=0; t<${TAG}; t++ ))
     do
@@ -14,7 +14,7 @@ do
         docker pull ${SRC}:${LOCAL_TAG}
         for (( d=0; d<${DST}; d++ ))
           do
-            TO=$(yq e '.images['"${c}"'].destination['"${d}"']' images.yml):${LOCAL_TAG}
+            TO=$(yq e '.images['"${c}"'].destinations['"${d}"']' images.yml):${LOCAL_TAG}
             docker tag ${SRC}:${LOCAL_TAG} ${TO}
             docker push ${TO}
             docker rmi ${TO}
