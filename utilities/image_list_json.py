4075bb04b5d297c942ecb92a7333fab0be8f8a0f
r3drun3 <simone.ragonesi@sighup.io>
2024-03-07 14:18:40 +0100

    chore(utilities): add python script to retrieve json image list
    Signed-off-by: r3drun3 <simone.ragonesi@sighup.io>

---
+++ utilities/image_list_json.py
@@ -0,0 +1,59 @@
+# USAGE python3 images_list_python.py --retrieve-last-3-tags (OPTIONAL)
+
+# OUTPUT SAMPLE:
+# [
+#   "registry.sighup.io/fury/acid/postgres-operator:v1.6.3",
+#   "registry.sighup.io/fury/acid/spilo-13:2.0-p7",
+#   "registry.sighup.io/fury/acid/pgbouncer:master-16",
+#   "registry.sighup.io/fury/acid/logical-backup:v1.6.3",
+#   "registry.sighup.io/fury/wrouesnel/postgres_exporter:v0.8.0",
+#   "registry.sighup.io/fury/grafana/tempo:2.3.1",
+#   "registry.sighup.io/fury/memcached:1.5.17-alpine",
+#   "registry.sighup.io/fury/velero/velero:v1.13.0"
+# ]
+
+import os
+import yaml
+import json
+import argparse
+
+def get_images_and_tags(directory, include_last_3_tags=False):
+    image_list = []
+
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == 'images.yml':
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    data = yaml.safe_load(f)
+                    for image_data in data.get('images', []):
+                        image_name = image_data.get('destinations', [''])[0]
+                        if image_name:
+                            tags = image_data.get('tag', [])
+                            if include_last_3_tags and len(tags) >= 3:
+                                tags = tags[-3:]
+                            else:
+                                tags = tags[-1:]  # Only the last tag
+                            image_list.extend([f"{image_name}:{tag}" for tag in tags])
+
+    return image_list
+
+def main():
+    parser = argparse.ArgumentParser(description='Process images YAML files.')
+    parser.add_argument('--retrieve-last-3-tags', action='store_true', help='Include the last 3 tags for each image')
+    args = parser.parse_args()
+
+    parent_folder = os.path.dirname(os.getcwd())
+    image_list = get_images_and_tags(parent_folder, include_last_3_tags=args.retrieve_last_3_tags)
+    with open('image_list.json', 'w') as f:
+        json.dump(image_list, f, indent=2)
+
+    # Print the content of the JSON file
+    with open('image_list.json', 'r') as f:
+        print(f.read())
+
+    # Delete the JSON file
+    os.remove('image_list.json')
+
+if __name__ == "__main__":
+    main()
