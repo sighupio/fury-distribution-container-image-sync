9b3fda89c149ca0acf15c6f2202da3bd30ca07d2
Samuele Chiocca <samuele@sighup.io>
2024-08-23 12:21:55 +0200

    feat: improve script

--- patch_images_with_copacetic.sh
+++ patch_images_with_copacetic.sh
@@ -14,21 +14,27 @@ file="to_be_patched.txt"
 
 docker run --detach --rm --privileged -p 127.0.0.1:8888:8888/tcp --name buildkitd --entrypoint buildkitd moby/buildkit:v0.11.4 --addr tcp://0.0.0.0:8888 # --platform linux/amd64
 
+echo "PATCH IMAGE REPORT:" > patch_report.txt
+
 while IFS= read -r line; do
     secured=$(echo "$line" | sed 's|registry.sighup.io/fury|registry.sighup.io/fury/secured|')
     docker pull $line # --platform linux/amd64
     trivy image -q --vuln-type os --ignore-unfixed -f json -o $(basename $line).json $line # --platform=linux/amd64
     if copa patch -r $(basename $line).json -i $line -a tcp://0.0.0.0:8888 ; then
-        echo "############## SUCCESS, will execute:"
+        echo "############## SUCCESS, we will execute:"
         echo "----> docker tag $line-patched $secured"
         echo "----> docker push $secured"
+        echo "$secured: SUCCESS" > patch_report.txt
     else
-        echo "%%%%%%%%%%%%%% COPA FAILED, will execute:"
+        echo "%%%%%%%%%%%%%% COPA FAILED, we will still execute:"
         echo "----> docker tag $line $secured"
         echo "----> docker push $secured"
+        echo "$secured: SKIPPED" > patch_report.txt
     fi
 
 done < "$file"
 
 docker stop buildkitd
 docker rm buildkitd
+
+cat patch_report.txt
