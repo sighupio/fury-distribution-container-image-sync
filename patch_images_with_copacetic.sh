d4fea65e8998bd87a9095f20e85de2810ce4b35d
Samuele Chiocca <samuele@sighup.io>
2024-08-23 12:45:49 +0200

    feat: fix output, improve printing

--- patch_images_with_copacetic.sh
+++ patch_images_with_copacetic.sh
@@ -18,23 +18,21 @@ echo "PATCH IMAGE REPORT:" > patch_report.txt
 
 while IFS= read -r line; do
     secured=$(echo "$line" | sed 's|registry.sighup.io/fury|registry.sighup.io/fury/secured|')
+    echo ">>>>>>>>>>>>>>>>>>> STARTING $line <<<<<<<<<<<<<<<<<<<<<"
     docker pull $line # --platform linux/amd64
     trivy image -q --vuln-type os --ignore-unfixed -f json -o $(basename $line).json $line # --platform=linux/amd64
     if copa patch -r $(basename $line).json -i $line -a tcp://0.0.0.0:8888 ; then
         echo "############## SUCCESS, we will execute:"
         echo "----> docker tag $line-patched $secured"
         echo "----> docker push $secured"
-        echo "$secured: SUCCESS" > patch_report.txt
+        echo "$secured: SUCCESS" >> patch_report.txt
     else
         echo "%%%%%%%%%%%%%% COPA FAILED, we will still execute:"
         echo "----> docker tag $line $secured"
         echo "----> docker push $secured"
-        echo "$secured: SKIPPED" > patch_report.txt
+        echo "$secured: SKIPPED" >> patch_report.txt
     fi
 
 done < "$file"
-
-docker stop buildkitd
-docker rm buildkitd
 
 cat patch_report.txt
