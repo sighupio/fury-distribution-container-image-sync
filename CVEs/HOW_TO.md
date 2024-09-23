aad51ca68418a479306fcf44200192715f2e1690
Giuseppe Iannelli <giuseppe.iannelli@sighup.io>
2024-09-23 10:53:31 +0200

    fixup! feat: proposal for automatic CVEs scanning and report of multiple KFD versions

--- CVEs/HOW_TO.md
+++ CVEs/HOW_TO.md
@@ -10,13 +10,14 @@ This guide explains how to generate a new report (in MarkDown) for a version of 
 
 ## Steps
 
-### Scan single KDF Version
+### Scan a single KFD Version
 
-1) Create a new folder with the name of the version of KFD and create a new `furyctl.yaml` file with cluster name `sighup` and the same distribution version (everything can be disabled, we only need to download dependencies)
+1) Create a new folder with the name of the version of KFD and create a new `furyctl.yaml` file with cluster name `sighup` and the same distribution version with kind KFDDistribution (everything can be disabled, we only need to download the dependencies): `furyctl create config --name sighup --version v1.X.Y --kind KFDDistribution --config v1.X.Y/furyctl.yaml`
 2) Execute `make download-deps KFD_VERSION=SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE` 
-3) Execute `make gen-image-list KFD_VERSION=SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE`, this command will output an `SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE/images.txt` file with all the images found in the build kustomize manifest.
-4) Execute `make scan-vulns KFD_VERSION=SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE`, this script will output a `SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE/README.md` file in the current directory with a table with all the CRITICAL CVEs
-5) Check the `SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE/README.md` and commit the directory
+3) Execute `make kustomize-build-all KFD_VERSION=SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE` 
+4) Execute `make gen-image-list KFD_VERSION=SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE`, this command will output an `SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE/images.txt` file with all the images found in the build kustomize manifest. 
+5) Execute `make scan-vulns KFD_VERSION=SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE`, this script will output a `SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE/README.md` file in the current directory with a table with all the CRITICAL CVEs 
+6) Check the `SOME_VALID_KFD_VERSION_WITH_A_FURYCTLYAML_INSIDE/README.md` and commit the directory
 
 ### Scan all KFD versions
 
--- scripts/scan_vuln.sh
+++ scripts/scan_vuln.sh
@@ -8,26 +8,32 @@ TRIVY_SCAN_OUTPUT_DIR=/tmp/kfd/${KFD_VERSION}
 SCAN_RESULT_OUTPUT_FILE=${KFD_VERSION}/README.md
 SCAN_ERROR_OUTPUT_FILE=${KFD_VERSION}/error.log
 
-echo "" > ${SCAN_ERROR_OUTPUT_FILE}
+echo "" > "${SCAN_ERROR_OUTPUT_FILE}"
 
-printf "# KFD ${KFD_VERSION}\n\n" > ${SCAN_RESULT_OUTPUT_FILE}
-printf "Updated to $(date +'%Y-%m-%d')\n\n" >> ${SCAN_RESULT_OUTPUT_FILE}
-printf "## CVEs\n\n" >> ${SCAN_RESULT_OUTPUT_FILE}
+{
+  printf "# KFD %s\n\n" "${KFD_VERSION}";
+  printf "Last updated %s\n\n" "$(date +'%Y-%m-%d')";
+  printf "## CVEs\n\n";
+} > "${SCAN_RESULT_OUTPUT_FILE}"
 
-echo "| Image | Severity | CVE | Reason | Package Affected | Status | Fixed in versions |" >> ${SCAN_RESULT_OUTPUT_FILE}
-echo "| --- | --- | --- | --- | --- | --- | --- |" >> ${SCAN_RESULT_OUTPUT_FILE}
+echo "| Image | Hash| Severity | CVE | Reason | Package Affected | Status | Fixed in versions |" >> "${SCAN_RESULT_OUTPUT_FILE}"
+echo "| --- | --- | --- | --- | --- | --- | --- | --- |" >> "${SCAN_RESULT_OUTPUT_FILE}"
 
-mkdir -p $TRIVY_SCAN_OUTPUT_DIR
+mkdir -p "$TRIVY_SCAN_OUTPUT_DIR"
 
 for image in $images_with_tags; do
   TRIVY_SCAN_OUTPUT_FILE=/tmp/kfd/${KFD_VERSION}/scan-${image//[:\/]/_}.json
-  trivy image --platform=linux/amd64  --no-progress --output $TRIVY_SCAN_OUTPUT_FILE --format json --severity CRITICAL "$image"
-  if [ $? -ne 0 ]; then
-    echo "$image | ERROR PROCESSING! " >> $SCAN_ERROR_OUTPUT_FILE
+
+  if ! trivy image --no-progress --output "$TRIVY_SCAN_OUTPUT_FILE" --format json --severity CRITICAL "$image"
+  then
+    echo "$image | ERROR PROCESSING! " >> "${SCAN_ERROR_OUTPUT_FILE}"
   else
-    cat $TRIVY_SCAN_OUTPUT_FILE | jq -r --arg image $image 'try .Results[].Vulnerabilities[] | "| " + $image + " | " + .Severity + " | " + .VulnerabilityID + " | " + .Title + " | " + .PkgName + " " + .InstalledVersion + " | " + .Status + " | " + .FixedVersion + " |" ' >> ${SCAN_RESULT_OUTPUT_FILE}
+    src_image_hash=$(cat $TRIVY_SCAN_OUTPUT_FILE | jq -r '.Metadata.ImageID')
+    jq -r --arg image "$image" \
+      --arg src_image_hash $src_image_hash \
+      'try .Results[].Vulnerabilities[] | "| " + $image + " | " + $src_image_hash +" | " + .Severity + " | " + .VulnerabilityID + " | " + .Title + " | " + .PkgName + " " + .InstalledVersion + " | " + .Status + " | " + .FixedVersion + " |" ' < "$TRIVY_SCAN_OUTPUT_FILE" >> "${SCAN_RESULT_OUTPUT_FILE}"
   fi
 
 done
 
-rm -rf $TRIVY_SCAN_OUTPUT_DIR
+rm -rf "$TRIVY_SCAN_OUTPUT_DIR"
